4b08d9dc58c7adfe9c4896d3da32a89638b380c8
pkgamma <pkgamma@gmail.com>
2018-10-02 23:15:27 -0500

    got shouldDisplayImage working

--- src/components/Instructions.component.js
+++ src/components/Instructions.component.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react'
 import './../styles/instructions.css'
 
 class Instructions extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { shouldDisplayImage: true }
+  }
+
   render() {
-    return (
-      <div className="instructions">
-        Follow the instructions on the README to get started!
-        <img
-          className="instructions__logo"
-          src="https://uiuc.hack4impact.org/img/colored-logo.png"
-          alt="h4i logo"
-        />
-      </div>
-    )
+    const shouldDisplayImage = this.state.shouldDisplayImage
+    if (shouldDisplayImage) {
+      return (
+        <div className="instructions">
+          Follow the instructions on the README to get started!
+          <img
+            className="instructions__logo"
+            src="https://uiuc.hack4impact.org/img/colored-logo.png"
+            alt="h4i logo"
+          />
+        </div>
+      )
+    }
   }
 }
 
